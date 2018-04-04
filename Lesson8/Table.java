8348418c5e30db7514bef14bf1d650f4907f863e
itotatsuhiko <37993270+itotatsuhiko@users.noreply.github.com>
2018-04-04 16:38:06 +0900

    [add] Lesson8の実施
    [add] Lesson8の実施

---
+++ Lesson8/Table.java
@@ -0,0 +1,21 @@
+package Lesson8;
+
+/**
+ * テーブルを表すインターフェース。
+ **/
+public interface Table {
+	/**
+	* カードを置く。
+	*テーブルに置かれたカードをどのように扱うかは,サブクラスで実装する。
+	*
+	* @param card カード
+	*/
+	public void putCard(Card card[]);
+
+	/**
+	 * カードを見る。
+	 *
+	 * @return テーブルに置かれたカードを表す配列
+	 **/
+	public Card [] [] getCard();
+}
